43f3215b30838c7fa845b64f21976ee19b29b9cb
Richard Nguyen <richardnguyeninquiries@gmail.com>
2017-10-02 10:42:12 -0400

    first commit

---
+++ DaPint/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DaPint.Startup))]
+namespace DaPint
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
